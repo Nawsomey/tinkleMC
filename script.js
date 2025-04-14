33385d590fc2a368b311b8a6894e1b681ddfaf5e
Nawsomey <111446394+Nawsomey@users.noreply.github.com>
2025-04-14 12:45:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ if (!username) {
 // UI elements
 const chatBox = document.getElementById("chat-messages");
 const msgInput = document.getElementById("msg");
-const sendBtn = document.getElementById("send-btn");
+const sendBtn = document.getElementById("send");
 
 // Display incoming messages
 onChildAdded(msgRef, (data) => {
